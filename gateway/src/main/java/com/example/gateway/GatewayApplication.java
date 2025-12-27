07d82f8d038492a6703933db69d7ce159ba0fafa
Denis.Kholopov <denis.holopov@krit.pro>
2025-12-27 13:28:12 +0300

    functional proxy

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,23 +1,26 @@
 package com.example.gateway;
 
+import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
 
-	@Bean
-	public RouteLocator myRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(r -> r.path("/**")
-						.uri("http://localhost:8081"))
-				.build();
-	}
+    @Bean
+    public RouteLocator myRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/**")
+                        .uri("http://localhost:8079"))
+                .build();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
